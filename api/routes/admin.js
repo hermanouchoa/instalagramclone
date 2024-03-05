202e8fa0a5159eaaf4b08372fb81c498d4874961
Hermano Uchoa <hermano.uchoa@icc.org.br>
2024-03-05 14:07:04 -0300

    rota para teste e conexão oracle

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -2,4 +2,9 @@ module.exports = function (api) {
     api.get('/ola', function (req, res) {
         api.controllers.admin.ola(api, req, res);    
     });    
+
+    api.get('/dual', function (req, res) {
+        api.controllers.admin.dual(api, req, res);    
+    });    
+       
 }
