84b26d9847bf7f8888fede9df6d1fa3fe5f0794a
Hermano Uchoa <hermano.uchoa@icc.org.br>
2024-03-06 07:34:57 -0300

    aplicando uso de variaveis para definir porta da aplicação

--- cliente_web_instagram_clone/app.js
+++ cliente_web_instagram_clone/app.js
@@ -2,6 +2,6 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
+app.listen(process.env.PRT_API, function(){
 	console.log('Servidor online');
 })
