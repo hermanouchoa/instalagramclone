5c47217dc8210bff87275eef835e4f98ba0e9399
Hermano Uchoa <hermano.uchoa@icc.org.br>
2024-03-13 13:24:09 -0300

    Aplicando uso de variaveis de ambiente para definicação de porta da API

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 var api = require('./config/server');
 
-var port = 3000;
+var port = process.env.PRT_API;
 api.listen(port, function (req, res) {
     console.log('Jesus me ajude nesse jornada. API escutando na '+port); 
 });
