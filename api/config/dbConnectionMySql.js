c475df8e5a43e76a2f071aaa1bba7faa2ea81e19
Hermano Uchoa <hermano.uchoa@icc.org.br>
2024-03-13 13:24:30 -0300

    Ajuste na classe de conexão com MySQL, usando async

--- api/config/dbConnectionMySql.js
+++ api/config/dbConnectionMySql.js
@@ -1,13 +1,15 @@
 var mysql = require('mysql');
 
-var connMysql = function () {
-    return mysql.createConnection({
+var connMysql = async function () {
+    var connection = await mysql.createConnection({
         host: process.env.HST_MYSQL,
         user: process.env.USR_MYSQL,
         password: process.env.PWD_MYSQL,
         database: process.env.DB_MYSQL,
         port: process.env.PRT_MYSQL,
     });
+
+    return connection;
 }
 
 module.exports = function () {
